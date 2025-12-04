39e6229c0dfc92664556fedcb7aee872ffbe9a0d
ghinkle <greghinkle@gmail.com>
2025-12-03 19:49:20 -0500

    cleanup: removing abandoned v2 collab implementation with overuse of yjs

--- packages/collabv2/migrations/0002_add_compression.sql
+++
@@ -1,7 +0,0 @@
--- Add compression flag to ydoc_snapshots table
--- This allows us to store gzip-compressed state vectors to work within D1's 2MB BLOB limit
-
-ALTER TABLE ydoc_snapshots ADD COLUMN compressed INTEGER DEFAULT 0 NOT NULL;
-
--- Create index on compressed column for monitoring compression usage
-CREATE INDEX idx_compressed ON ydoc_snapshots(compressed);
